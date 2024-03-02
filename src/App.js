27a30314a732ead84a774288f2e839acba2ae105
Adrian <adriannasrat@gmail.com>
2024-03-02 21:42:19 +0100

    added navbar, and icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import React, { useState, useEffect } from 'react';
-function App() {
+import "./index.css"
+import "./output.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon as fasFaMoon } from '@fortawesome/free-solid-svg-icons';
+import { faMoon as farFaMoon } from '@fortawesome/free-regular-svg-icons';
+import { faBook as book } from '@fortawesome/free-solid-svg-icons';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+const getSystemColorScheme = () => window.matchMedia('(prefers-color-scheme: dark').matches ? 'dark' : 'light';
+
+const App = () => {
+  const storedTheme = localStorage.getItem('theme')
+  const defaultTheme = storedTheme || getSystemColorScheme();
+  const [theme, setTheme] = useState(defaultTheme);
+
+  const swtichTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    document.documentElement.classList.toggle('dark', newTheme === 'dark')
+    localStorage.setItem('theme', newTheme);
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    document.documentElement.classList.toggle('dark', theme === 'dark');
+  }, [theme]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <Router>
+      <div className={`App ${theme === 'dark' ? 'dark' : ''}} bg-white dark:bg-darkBg`}>
+        <nav className="shadow-lg bg-white dark:bg-darkElement ">
+          <div className="max-w-screen-2xl mx-auto py-6 items-center">
+            <div className="flex justify-between  dark:text-white sm:px-20 px-10">
+              <Link to="/" className="md:text-2xl text-lg font-bold"><FontAwesomeIcon icon={book} /></Link>
+              <button className="flex items-center gap-2 md:gap-4" onClick={swtichTheme}>
+                <FontAwesomeIcon icon={theme === 'dark' ? fasFaMoon : farFaMoon} />
+                <span className="md:text-xl">Dark Mode</span>
+              </button>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </Router>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,js,jsx}"
+  ],
+  darkMode: "class",
   theme: {
+    colors: {
+      "white": "hsl(0, 0%, 100%)",
+      "darkElement": "hsl(209, 23%, 22%)",
+      "darkBg": "hsl(207, 26%, 17%)",
+      "darkGray": "hsl(0, 0%, 52%)",
+      "lightGray": " hsl(0, 0%, 98%)"
+    },
     extend: {},
   },
   plugins: [],
