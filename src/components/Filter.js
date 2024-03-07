914de3d27a96c6a4a60f641ca4196a6d1ebbc841
Adrian <adriannasrat@gmail.com>
2024-03-07 07:49:02 +0100

    added filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const Filter = ({ handleWordChange, filterValue }) => {
   const placeholderText = 'Search for a word...';
